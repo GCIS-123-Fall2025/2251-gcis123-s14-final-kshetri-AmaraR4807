5e0aa94de1e1701520896fa5cce9de727fb728da
Amara Ramm (RIT Student) <aer5363@rit.edu>
2025-12-15 09:25:53 -0500

    question 1, not fully working

--- shoppers.py
+++ shoppers.py
@@ -18,11 +18,40 @@ Write down the manual test by creating at least two items.
 """
 
 class Item:
-    pass # please replace with your solution
+  __slots__ = ['__code', '__name', '__price']
+  def __init__(self, code, name, price):
+    self.__code = code
+    self.__name = name
+    self.__price = price
 
-
+    def get_code(self):
+      return self.__code
+    def get_name(self):
+      return self.__name
+    def get_price(self):
+      return self.__price
+    
+    def set_code(self, code):
+      self.__code = code
+    def set_name(self, name):
+      self.__name = name
+    def set_price(self, price):
+      self.__price = price
+    
+    def print_item(self):
+      print(self.__code, "\t", self.__name, "\t", self.__price, sep="")
+    def __repr__(self):
+      return "item(code=" + self.__code + "name=" + self.__name + "price=" + str(self.__price) + ")"
+    def __str__(self):
+      return self.__name + " [" + self.__code + "] - $" + str(self.__price)
+    
 # manual test from main() method
-def main():     pass
- 
+def main():
+  item1 = "ABCD-1234", "Pencil", "1.10"
+  item2 = "CDEF-4567", "Binder", "3.55"
+  item1.print_item()
+  item2.print_item()
+  print(item1) # output from __str__
+  print(repr(item2)) # uses repr
 
 if __name__ == "__main__":    main()
